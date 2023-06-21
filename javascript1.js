f749a6903f6bcaeb8e738b601faa24ff28c4ac92
Aartirathee <aartirathee608@gmail.com>
2023-06-21 14:26:10 +0530

    update it

--- javascript1.js
+++ javascript1.js
@@ -1,4 +1,6 @@
-function toggleMenu() {
-  var menu = document.getElementById('menu-items');
-  menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
-}
+// Set the width of the progress bar dynamically
+const skills = document.querySelectorAll('.skill-progress');
+skills.forEach(skill => {
+  const width = skill.parentNode.querySelector('.skill-percent').textContent;
+  skill.style.setProperty('--progress-width', width);
+});
